a0095807c46a92de84af113b39b532605bc1a7b1
sajith231 <sajiththomas231@gmail.com>
2025-10-23 13:01:06 +0530

    sales and purchase api

--- task_backend/settings.py
+++ task_backend/settings.py
@@ -170,7 +170,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-Cloudinary Configuration
+
 cloudinary.config(
     cloud_name=config("CLOUDINARY_CLOUD_NAME"),
     api_key=config("CLOUDINARY_API_KEY"),
