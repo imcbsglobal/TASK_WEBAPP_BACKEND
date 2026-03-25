631819ee37c25be34c7ccd2af19c8afcaa895289
sreekuttyu <111846712+sreekuttyu@users.noreply.github.com>
2026-03-25 10:36:11 +0530

    Refactor sales endpoints: rename get_sales_today to get_sales_today_usersummary and add new endpoints for typewise and detailed sales summaries

--- salestoday_purchasetoday/urls.py
+++ salestoday_purchasetoday/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('salestoday/', views.get_sales_today, name='get_sales_today'),
+    path('get_sales_today_usersummary/', views.get_sales_today_usersummary, name='get_sales_today_usersummary'),
     path('purchasetoday/', views.get_purchase_today, name='get_purchase_today'),
     path('salesdaywise/', views.get_sales_daywise, name='get_sales_daywise'),
     path('salesmonthwise/', views.get_sales_monthwise, name='get_sales_monthwise'),
-    path('get-sale-report/', views.get_sale_report, name='get_sale_report'),
-    path('salestoday-usersummary/', views.get_sales_today_usersummary, name='get_sales_today_usersummary'),
+    path('salestoday-typewise/', views.get_sales_today_typewise, name='get_sales_today_typewise'),
+    path('salestoday-details/', views.get_sales_today_details, name='get_sales_today_details'),
 ]
-# s
